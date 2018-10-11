12a5b30d345fc1c0266883137d54f66ddb413009
Renan Sies <renansies@gmail.com>
2018-10-11 20:20:51 -0300

    Enemy Health and Player Shooting

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,29 +5,29 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     private Transform _player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
     private NavMeshAgent _nav;
 
 
     private void Awake ()
     {
         _player = GameObject.FindGameObjectWithTag ("Player").transform;
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent <EnemyHealth> ();
+        playerHealth = _player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
         _nav = GetComponent <NavMeshAgent> ();
     }
 
 
     void Update ()
     {
-        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
             _nav.SetDestination (_player.position);
-        //}
-        //else
-        //{
-        //    nav.enabled = false;
-        //}
+        }
+        else
+        {
+            _nav.enabled = false;
+        }
     }
 }
